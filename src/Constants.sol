b24dfc40a43b7573331f7bb1eac36cf9d06c361e
RonexLemon <ronexondimu@gmail.com>
2024-05-02 06:36:59 +0300

    feat:constants

---
+++ src/Constants.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+/**
+* Constants are values that can't be changed, and their value are hardcoded
+*using constant can save gas
+*/
+
+contract Constant{
+
+    //coding conversion to uppercase constant variables
+
+    address public constant MyAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    uint256 public constant PRICE = 0.1 ether;
+}
